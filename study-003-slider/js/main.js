a2cb887e0217b413195b45cfbfc7a550c8b55c5c
sbpark88 <saebyulpark88@gmail.com>
2023-03-26 11:00:22 +0900

    Update: Decorate slider buttons styling

--- study-003-slider/js/main.js
+++ study-003-slider/js/main.js
@@ -5,6 +5,10 @@ const panelWidth = parseInt(window.getComputedStyle(panels[0])['width'])
 const buttonWrapper = document.getElementsByClassName('btn')[0]
 const buttons = [...buttonWrapper.getElementsByTagName('li')]
 
+const THOUSAND = 1000
+const seconds = 1
+const motionTime = THOUSAND * seconds
+
 buttonWrapper.addEventListener('click', (evt) => {
   // condition 1 : '<ul class="btn">'이 아닌 '<li class="btn-wrapper__btn-number"></li>'가 클릭된 게 맞는가?
   // condition 2 : 현재 활성 상태가 아닌가?(= on 이 아닌가?)
@@ -19,7 +23,7 @@ const slidePanels = (index) => {
   new Anime(panelWrapper, {
     prop: 'margin-left',
     value: `${index * (-100)}`,
-    duration: 500
+    duration: motionTime
   })
 }
 const setButtonToggle = (index) => {
