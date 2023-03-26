512f37301efdcb4f69f5bd92dfa2047e1a356e50
sbpark88 <saebyulpark88@gmail.com>
2023-03-26 10:20:10 +0900

    Update: Move slider on button click

---
+++ study-003-slider/js/main.js
@@ -0,0 +1,31 @@
+const panelWrapper = document.getElementsByClassName('panel')[0]
+const panels = [...panelWrapper.getElementsByTagName('div')]
+const panelWidth = parseInt(window.getComputedStyle(panels[0])['width'])
+
+const buttonWrapper = document.getElementsByClassName('btn')[0]
+const buttons = [...buttonWrapper.getElementsByTagName('li')]
+
+buttonWrapper.addEventListener('click', (evt) => {
+  // condition 1 : '<ul class="btn">'이 아닌 '<li class="btn-wrapper__btn-number"></li>'가 클릭된 게 맞는가?
+  // condition 2 : 현재 활성 상태가 아닌가?(= on 이 아닌가?)
+  if ((buttons.some(v => v === evt.target)) && (!evt.target.classList.contains('on'))) {
+    const btnIndex = Number(evt.target.dataset.number)
+    slidePanels(btnIndex)
+    setButtonToggle(btnIndex)
+  }
+})
+
+const slidePanels = (index) => {
+  new Anime(panelWrapper, {
+    prop: 'margin-left',
+    value: `${index * (-100)}`,
+    duration: 500
+  })
+}
+const setButtonToggle = (index) => {
+  buttons.forEach((v, i) => {
+    i === index
+        ? (v.classList.add('on'))
+        : (v.classList.remove('on'))
+  })
+}
