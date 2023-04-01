d64bd8fbb036a12d9cec1caab1e8baaba404e58a
sbpark88 <saebyulpark88@gmail.com>
2023-04-01 14:57:08 +0900

    Update: Change frame height by box height

--- study-005-tab-ex-master/js/main.js
+++ study-005-tab-ex-master/js/main.js
@@ -1,15 +1,31 @@
 const main = document.getElementsByTagName('main')[0]
+const btnsWrapper = document.getElementById('wrapper__btns')
 const btns = document.querySelectorAll(' ul li')
 const boxs = main.querySelectorAll('section article')
 
-btns.forEach((el, idx) => {
-  el.addEventListener('click', (e) => {
-    e.preventDefault();
 
-    for (const btn of btns) btn.classList.remove('on');
-    btns[idx].classList.add('on');
+btnsWrapper.addEventListener('click', (e) => {
+  e.preventDefault()
+  const index = Number(e.target.dataset.number) - 1;
+  if (btns[index].classList.contains('on')) return;   // 활성 타겟 재클릭 동작 방지
+  [btns, boxs].forEach(arr => activation(arr, index)) // 비활성 타겟 클릭시 동작
+  changeHeight(index)
+})
 
-    for (const box of boxs) box.classList.remove('on');
-    boxs[idx].classList.add('on');
-  });
-});
+// MARK: TAB Click
+const activation = (arr, index) => {
+  console.log('activation')
+  for (const el of arr) {
+    el.classList.remove('on')
+  }
+  arr[index].classList.add('on')
+}
+
+const changeHeight = index => {
+  // 해당 높이값으로 탭 프레임의 높이값 모션 적용
+  new Anime(main, {
+    prop: 'height',
+    value: parseInt(`${getComputedStyle(boxs[index])?.height}`),
+    duration: 500
+  })
+}
