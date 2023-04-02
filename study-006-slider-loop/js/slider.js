9e42fae3009814f6f0ea843158daf7c4bfe5386c
sbpark88 <saebyulpark88@gmail.com>
2023-04-02 13:27:32 +0900

    Update: Append init function
    prev 스크롤을 동작시키도록 하기 위해 항상 앞뒤로
    최소 1개 이상의 elements 가 존재해야한다.
    이를 위해 js가 로딩되자마자 마지막 element 하나를
    앞에 위치시키고 두 번째 element 가 보이도록 left 에
    -100% 를 주고, 개수가 늘어나도 동작하도록 전체 width 를
    게산해 넣도록 한다.

--- study-006-slider-loop/js/slider.js
+++ study-006-slider-loop/js/slider.js
@@ -5,6 +5,14 @@ const ul = slider.getElementsByTagName('ul')[0]
 const prev = document.getElementsByClassName('prev')[0]
 const next = document.getElementsByClassName('next')[0]
 
+// init
+;(() => {
+  ul.style.left = '-100%';
+  ul.prepend(ul.lastElementChild);
+  ul.style.width = `${100  * ul.children.length}%`; // dynamic ul width
+  [...ul.children].forEach(el => el.style.width = `${100 / ul.children.length}%`);  // dynamic li width
+})()
+
 next.addEventListener('click', () => slideToNext(ul))
 prev.addEventListener('click', () => slideToPrev(ul))
 
@@ -12,7 +20,7 @@ function slideToNext(el) {
   new Anime(el, {
     prop: 'left',
     value: '-200%',
-    duration: 5000,
+    duration: 1500,
     callback: () => {
       el.append(el.firstElementChild);
       el.style.left = '-100%'
@@ -24,7 +32,7 @@ function slideToPrev(el) {
   new Anime(el, {
     prop: 'left',
     value: '0',
-    duration: 5000,
+    duration: 1500,
     callback: () => {
       el.prepend(el.lastElementChild);
       el.style.left = '-100%'
