ed720af243f9d490047de94dae99723eeaa6064d
sbpark88 <saebyulpark88@gmail.com>
2023-04-02 14:26:06 +0900

    Update: Create Test Data
    and Change full screen

--- study-006-slider-loop/js/slider.js
+++ study-006-slider-loop/js/slider.js
@@ -4,10 +4,29 @@ const slider = document.getElementById('slider');
 const ul = slider.getElementsByTagName('ul')[0];
 const prev = document.getElementsByClassName('prev')[0];
 const next = document.getElementsByClassName('next')[0];
+const speed = 500
 let enableEvent = true;
+
+// MARK: test data
+const data = [
+  { txt: 'TEXT1', bg: 'hotpink'},
+  { txt: 'TEXT2', bg: 'orange'},
+  { txt: 'TEXT3', bg: 'aqua'},
+  { txt: 'TEXT4', bg: 'greenyellow'},
+  { txt: 'TEXT5', bg: 'blue'},
+]
+
+const createDOM = (data, parentEl) => {
+      let tags = '';
+      data.map((v, i) => {
+        tags += `<li data-index="${i + 1}" style="background-color: ${v.bg}">${v.txt}</li>`
+      })
+      parentEl.innerHTML = tags
+    }
 
 // init
 ;(() => {
+  createDOM(data, ul);
   ul.style.left = '-100%';
   ul.prepend(ul.lastElementChild);
   ul.style.width = `${100 * ul.children.length}%`; // dynamic ul width
@@ -24,7 +43,7 @@ function slideToNext(el) {
   new Anime(el, {
     prop: 'left',
     value: '-200%',
-    duration: 1500,
+    duration: speed,
     callback: () => {
       el.append(el.firstElementChild);
       el.style.left = '-100%'
@@ -38,7 +57,7 @@ function slideToPrev(el) {
   new Anime(el, {
     prop: 'left',
     value: '0',
-    duration: 1500,
+    duration: speed,
     callback: () => {
       el.prepend(el.lastElementChild);
       el.style.left = '-100%'
@@ -46,4 +65,3 @@ function slideToPrev(el) {
     }
   })
 }
-
