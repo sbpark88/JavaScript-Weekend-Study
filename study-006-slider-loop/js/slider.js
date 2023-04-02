f33f736480785eaa9e74ed6c2750b3d70150c2d4
sbpark88 <saebyulpark88@gmail.com>
2023-04-02 13:51:31 +0900

    Update: Prevent event duplication in motion running

--- study-006-slider-loop/js/slider.js
+++ study-006-slider-loop/js/slider.js
@@ -1,22 +1,26 @@
 // 'use strict'
 
-const slider = document.getElementById('slider')
-const ul = slider.getElementsByTagName('ul')[0]
-const prev = document.getElementsByClassName('prev')[0]
-const next = document.getElementsByClassName('next')[0]
+const slider = document.getElementById('slider');
+const ul = slider.getElementsByTagName('ul')[0];
+const prev = document.getElementsByClassName('prev')[0];
+const next = document.getElementsByClassName('next')[0];
+let enableEvent = true;
 
 // init
 ;(() => {
   ul.style.left = '-100%';
   ul.prepend(ul.lastElementChild);
-  ul.style.width = `${100  * ul.children.length}%`; // dynamic ul width
+  ul.style.width = `${100 * ul.children.length}%`; // dynamic ul width
   [...ul.children].forEach(el => el.style.width = `${100 / ul.children.length}%`);  // dynamic li width
 })()
 
-next.addEventListener('click', () => slideToNext(ul))
-prev.addEventListener('click', () => slideToPrev(ul))
+window.onload = () => {
+  next.addEventListener('click', () => enableEvent && slideToNext(ul))
+  prev.addEventListener('click', () => enableEvent && slideToPrev(ul))
+}
 
 function slideToNext(el) {
+  enableEvent = false
   new Anime(el, {
     prop: 'left',
     value: '-200%',
@@ -24,11 +28,13 @@ function slideToNext(el) {
     callback: () => {
       el.append(el.firstElementChild);
       el.style.left = '-100%'
+      enableEvent = true
     }
   })
 }
 
 function slideToPrev(el) {
+  enableEvent = false
   new Anime(el, {
     prop: 'left',
     value: '0',
@@ -36,6 +42,7 @@ function slideToPrev(el) {
     callback: () => {
       el.prepend(el.lastElementChild);
       el.style.left = '-100%'
+      enableEvent = true
     }
   })
 }
