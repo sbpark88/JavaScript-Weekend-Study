c945139286c250f105f356c85c85cda3b0ac102b
sbpark88 <saebyulpark88@gmail.com>
2023-04-02 12:57:35 +0900

    Update: Append Feature - Slide to next

--- study-006-slider-loop/js/slider.js
+++ study-006-slider-loop/js/slider.js
@@ -1,1 +1,20 @@
-'use strict'
+// 'use strict'
+
+const slider = document.getElementById('slider')
+const ul = slider.getElementsByTagName('ul')[0]
+const prev = document.getElementsByClassName('prev')[0]
+const next = document.getElementsByClassName('next')[0]
+
+next.addEventListener('click', () => slideToNext(ul))
+function slideToNext(el) {
+  new Anime(el, {
+    prop: 'left',
+    value: '-100%',
+    duration: 500,
+    callback: () => {
+      ul.append(ul.firstElementChild);
+      ul.style.left = '0'
+    }
+  })
+}
+
