cc6f9779e3307522c126e168bf7aad362994a835
sbpark88 <saebyulpark88@gmail.com>
2023-04-17 12:56:57 +0900

    Update: 'this' in ES5 Functions
    bind...bind...bind...

--- study-012-tab-es5/js/main.js
+++ study-012-tab-es5/js/main.js
@@ -1,19 +1,29 @@
-var tab = document.querySelector('#tab');
-var btns = tab.querySelectorAll('ul li');
-var boxs = tab.querySelectorAll('article div');
+function Tab(selector) {
+  this.tab = document.querySelector(selector)
+  this.btns = this.tab.querySelectorAll('ul li')
+  this.boxs = this.tab.querySelectorAll('article div')
+  this.addTabEvent()
+}
 
-addTabEvent()
-function addTabEvent() {
-  btns.forEach(function (btn, idx) {
-    btn.addEventListener('click', function () {
-      [btns, boxs].forEach((el) => activation(el, idx));
+Object.defineProperty(Tab.prototype, 'addTabEvent', {
+  value: function () {
+    this.btns.forEach(function (btn, idx) {
+      btn.addEventListener('click', function () {
+        [this.btns, this.boxs].forEach(function (el) {
+          this.activation(el, idx)
+        }.bind(this)) // What the.........
+      }.bind(this))
+    }.bind(this))
+  },
+})
+
+Object.defineProperty(Tab.prototype, 'activation', {
+  value: function (arr, idx) {
+    arr.forEach(function (el) {
+      el.classList.remove('on');
     });
-  });
-}
+    arr[idx].classList.add('on');
+  }
+})
 
-function activation(arr, idx) {
-  arr.forEach(function (el) {
-    el.classList.remove('on');
-  });
-  arr[idx].classList.add('on');
-}
+new Tab('#tab')
