e8bb969f0fc72e20e9dd0235da00cdba107b5eb5
sbpark88 <saebyulpark88@gmail.com>
2023-04-23 14:58:57 +0900

    Update: Asynchronous - ES6 Promise

--- study-014-asynchronous/docs/Asynchronous.md
+++ study-014-asynchronous/docs/Asynchronous.md
@@ -1,4 +1,4 @@
-# ES5 Callback Hell
+# 1. ES5 Callback Hell
 
 ```javascript
 const delay = 1000
@@ -42,3 +42,80 @@ end
 
 ---
 
+# 2. ES6 Promise
+
+```javascript
+const delay = 1000
+
+const es_promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('success')
+  }, delay)
+})
+
+
+console.log('start')
+
+es_promise
+    .then(response => {
+      console.log("I'm callback 1.")
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('success')
+        }, delay)
+      })
+    })
+    .then(response => {
+      console.log("I'm callback 2.")
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('success')
+        }, delay)
+      })
+    })
+    .then(response => {
+      console.log("I'm callback 3.")
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          reject('fail')
+        }, delay)
+      })
+    })
+    .then(response => {
+      console.log("I'm callback 4.")
+      console.debug(response)
+    })
+    .catch(response => {
+      console.log("I'm callback 4.")
+      console.debug(response)
+    })
+    .finally(() => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('success')
+        }, delay)
+      })
+    })
+    .then(response => {
+      console.log("I'm callback 5.")
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('success')
+        }, delay)
+      })
+    })
+    .finally(() => console.log('end'))
+```
+
+```console
+start
+I'm callback 1.
+I'm callback 2.
+I'm callback 3.
+I'm callback 4.
+`fail`
+I'm callback 5.
+end
+```
+
+🥵🥵🥵
--- study-014-asynchronous/js/main.js
+++ study-014-asynchronous/js/main.js
@@ -1,26 +1,58 @@
 const delay = 1000
-function es5_callback_hell(delay, callback) {
-  setTimeout(function () {
-    callback()
+const es_promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('success')
   }, delay)
-}
+})
 
 console.log('start')
 
-es5_callback_hell(delay, function () {
-  console.log("I'm callback 1.")
-  es5_callback_hell(delay, function () {
-    console.log("I'm callback 2.")
-    es5_callback_hell(delay, function () {
-      console.log("I'm callback 3.")
-      es5_callback_hell(delay, function () {
-        console.log("I'm callback 4.")
-        es5_callback_hell(delay, function () {
-          console.log("I'm callback 5.")
-          console.log('end')
-        })
+es_promise
+    .then(response => {
+      console.log("I'm callback 1.")
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('success')
+        }, delay)
       })
     })
-  })
-})
-
+    .then(response => {
+      console.log("I'm callback 2.")
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('success')
+        }, delay)
+      })
+    })
+    .then(response => {
+      console.log("I'm callback 3.")
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          reject('fail')
+        }, delay)
+      })
+    })
+    .then(response => {
+      console.log("I'm callback 4.")
+      console.debug(response)
+    })
+    .catch(response => {
+      console.log("I'm callback 4.")
+      console.debug(response)
+    })
+    .finally(() => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('success')
+        }, delay)
+      })
+    })
+    .then(response => {
+      console.log("I'm callback 5.")
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('success')
+        }, delay)
+      })
+    })
+    .finally(() => console.log('end'))
