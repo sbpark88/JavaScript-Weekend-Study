368977f9bdf4dc12119afaea67589036a33cbdc2
sbpark88 <saebyulpark88@gmail.com>
2023-04-23 12:50:04 +0900

    Update: RAF 2nd - {value, seconds} 를 받아 처리

--- study-013-request-animation-frame/js/anime.js
+++ study-013-request-animation-frame/js/anime.js
@@ -2,16 +2,22 @@ import {$, eventBind, clickEventBind} from "./domUtil.js";
 
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
-let num = 0;
 clickEventBind('button')(() => requestAnimationFrame(moveSquare))
 
-const move = target => () => {
-  if (num < 200) {
-    ++num
-    requestAnimationFrame(moveSquare)
+const move = target => ({value, seconds}) => {
+  let start = null
+  const milliseconds = seconds * 1000
+  return (timestamp) => {
+    if (!start) start = timestamp
+    let progress = timestamp - start
+    if (progress < milliseconds) {
+      target.style.marginLeft = `${value * progress / milliseconds}px`
+      requestAnimationFrame(moveSquare)
+    } else {  // 주어진 정수값에 딱 떨어지도록 하기 위해... 이렇게 안 해주면 500 입력시 499.98 이런식으로 멈춘다.
+      target.style.marginLeft = `${value}px`
+    }
   }
-  console.log(`${num}px`)
-  target.style.marginLeft = `${num}px`
 }
 
-const moveSquare = move(box)
+const moveSquare = move(box)({value: 500, seconds: 2})
+
