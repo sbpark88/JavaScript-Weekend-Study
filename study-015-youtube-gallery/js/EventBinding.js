3845699d5ac71b359f9eaf2529be68f548a37878
sbpark88 <saebyulpark88@gmail.com>
2023-04-29 15:02:31 +0900

    Add: Render, EventBinding, FP utils

--- study-015-youtube-gallery/js/EventBinding.js
+++ study-015-youtube-gallery/js/EventBinding.js
@@ -1,13 +1,9 @@
-const $ = selector => {
-  const nodeList = document.querySelectorAll(selector)
-  return nodeList.length === 1 ? nodeList[0] : nodeList
-}
+import {$} from "./Render.js";
 
 const eventBind = el => eventType => listener => $(el).addEventListener(eventType, listener)
 const clickEventBind = el => listener => $(el).addEventListener('click', listener)
 
 export {
-  $,
   eventBind,
-  clickEventBind
+  clickEventBind,
 }
