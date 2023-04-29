c386e2a22fb454dcfee9edbc49f817a383376b1b
sbpark88 <saebyulpark88@gmail.com>
2023-04-29 14:44:43 +0900

    Feat: Act 3 Steps. Fetch Data, Render, Event Binding

--- study-015-youtube-gallery/js/main.js
+++ study-015-youtube-gallery/js/main.js
@@ -1,22 +1,50 @@
-const key = ''
+import {$, eventBind, clickEventBind} from './domUtil.js'
+import {apiKey} from "../../private/youtube-api.js";
+
+const key = apiKey
 const list = 'PLRROPbx6xj0H9H4jmTmQ1RS1LoJkG5Jfe'
 const num = 10
 /* Ref: https://developers.google.com/youtube/v3/docs/?hl=ko#PlaylistItems */
 const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&maxResults=${num}&playlistId=${list}`
-const main = document.querySelector('main')
+const main = $('main')
+
+// MARK: Data
 
 fetch(url)
     .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      let tags = ''
-      data.items.forEach(item => {
-        tags +=
-            `
+    .then(data => renderVideoList(data))
+
+// MARK: Render
+
+const renderVideoList = data => {
+  let tags = ''
+  data.items.forEach(item => {
+    const snippet = item.snippet
+    const title = snippet.title.length > 50 ? snippet.title : snippet.title.slice(0, 50) + "..."
+    const description = snippet.description > 100 ? snippet.description : snippet.description.slice(0, 100) + "..."
+    tags +=
+        `
             <article>
-              <h2>${item.snippet.title}</h2>
+              <a class="picture" href="${snippet.resourceId.videoId}">
+                <img src="${snippet.thumbnails.standard.url}" alt=${title}>
+              </a>
+              <div class="content">
+                <h3>Title:  ${title}</h3>
+                <p>${description}</p>
+                <span>${snippet.publishedAt.slice(0, 10)}</span>
+              </div>
             </article>
             `
-      })
-      main.innerHTML = tags
-    })
+  })
+  main.innerHTML = tags
+}
+
+// MARK: Event Binding
+clickEventBind('main')(event => {
+  event.preventDefault()
+  if (!(event.target.parentNode.nodeName === 'A')) return
+  const videoId = event.target.closest('a').getAttribute('href')
+  console.log(videoId)
+})
+
+
