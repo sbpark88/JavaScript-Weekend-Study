56bd3496500f450bebf7ffb0b9b0a8a1992edad1
sbpark88 <saebyulpark88@gmail.com>
2023-05-07 15:02:12 +0900

    Update: Set style and button event binding

--- study-017-text-animation/js/main.js
+++ study-017-text-animation/js/main.js
@@ -1,1 +1,14 @@
 import {$, renderInnerHTML} from "./Render.js";
+import {clickEventBind} from "./EventBinding.js";
+
+const buttonWrapper = $('ul')
+const buttons = $('li')
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', event => {
+    event.preventDefault()
+
+    for (const el of buttons) el.classList.remove('on')
+    buttons[index].classList.add('on')
+  })
+})
