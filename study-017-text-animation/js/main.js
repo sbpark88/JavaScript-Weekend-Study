9b03bca16a330874fdeef57c8e90fcb0e501f5a4
sbpark88 <saebyulpark88@gmail.com>
2023-05-12 15:25:24 +0900

    Update: Change the string to character is wrapped by span.

--- study-017-text-animation/js/main.js
+++ study-017-text-animation/js/main.js
@@ -1,14 +1,36 @@
 import {$, renderInnerHTML} from "./Render.js";
 import {eventBind} from "./EventBinding.js";
 
-const buttonWrapper = $('ul')
-const buttons = $('li')
+// MARK: Page Init
+//       article 안쪽 h1 의 문자를 각각 <span> 태그로 감싸주기
+$('article h1').forEach(hTag => {
+  const rawText = hTag.textContent
+  const template = [...rawText].reduce((acc, character) => acc += wrapSpan(character), '')
+  hTag.innerHTML = template
 
-buttons.forEach((button, index) => {
-  button.addEventListener('click', event => {
-    event.preventDefault()
+  function wrapSpan(text) {
+    return `<span>${text}</span>`
+  }
+})
 
-    for (const el of buttons) el.classList.remove('on')
-    buttons[index].classList.add('on')
-  })
+const careerDesigner = $('.career__designer')
+const careerDeveloper = $('.career__developer')
+
+const toggleOnDesigner = () => {
+  careerDesigner.forEach(el => el.classList.toggle('on', true))
+  careerDeveloper.forEach(el => el.classList.toggle('on', false))
+}
+
+const toggleOnDeveloper = () => {
+  careerDesigner.forEach(el => el.classList.toggle('on', false))
+  careerDeveloper.forEach(el => el.classList.toggle('on', true))
+}
+
+const ActivationToggle = {
+  btnDesigner: toggleOnDesigner,
+  btnDeveloper: toggleOnDeveloper
+}
+
+eventBind('.main__btn', 'click', event => {
+  ActivationToggle[event.target.id]()
 })
