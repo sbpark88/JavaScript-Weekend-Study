d0c3361ecbd0e3d7962f351caf9e74b628459e9b
sbpark88 <saebyulpark88@gmail.com>
2023-05-13 21:55:17 +0900

    Refactor: Button click motion styling
    버튼 클릭시 일시적으로 버튼 확대 및 핵상 변경

--- study-019-loop-slider/js/main.js
+++ study-019-loop-slider/js/main.js
@@ -2,6 +2,7 @@
 
 import {$} from './Render.js'
 import {eventBind} from "./EventBinding.js";
+import {escapingClosure} from "./CommonUtils.js";
 
 const frame = $('.list')
 
@@ -9,6 +10,9 @@ const frame = $('.list')
 const clickPrev = (target, eventBind) => {
   eventBind('.prev', 'click', () => {
     target.prepend(target.lastElementChild)
+    const prevButton = $('.prev')
+    prevButton.classList.toggle('on', true)
+    escapingClosure(() => prevButton.classList.toggle('on', false), 0.5)
   })
 }
 clickPrev(frame, eventBind)
@@ -16,6 +20,9 @@ clickPrev(frame, eventBind)
 const clickNext = (target, eventBind) => {
   eventBind('.next', 'click', () => {
     target.append(target.firstElementChild)
+    const nextButton = $('.next')
+    nextButton.classList.toggle('on', true)
+    escapingClosure(() => nextButton.classList.toggle('on', false), 0.5)
   })
 }
 clickNext(frame, eventBind)
