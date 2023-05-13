09cc260c5bf4ef6cb94aa4a5d76c6277a53e77be
sbpark88 <saebyulpark88@gmail.com>
2023-05-13 11:31:12 +0900

    Update: Rotate String to clockwise

--- study-017-text-animation/js/main.js
+++ study-017-text-animation/js/main.js
@@ -1,12 +1,26 @@
 import {$, renderInnerHTML} from "./Render.js";
 import {eventBind} from "./EventBinding.js";
 
+const circle = document.querySelector('.circle p');
+
+circle.innerHTML = circle.innerText
+    .split('')
+    .map(
+        (letter, idx) =>
+            `<span style='transform:rotate(${idx * 6}deg) translateY(-150px)'>${letter}</span>`
+    )
+    .join('');
+
+
+
 // MARK: Page Init
 //       article 안쪽 h1 의 문자를 각각 <span> 태그로 감싸주기
 $('article h1').forEach(hTag => {
   const rawText = hTag.textContent
   const template = [...rawText].reduce((acc, character) => acc += wrapSpan(character), '')
   hTag.innerHTML = template
+
+  hTag.querySelectorAll('span').forEach((el, index) => el.style.transitionDelay = `${0.1 * index}s`)
 
   function wrapSpan(text) {
     return `<span>${text}</span>`
