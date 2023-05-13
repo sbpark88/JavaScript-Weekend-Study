62f6d5d6f07099b08655dfb571cdb7ecb284d4a0
sbpark88 <saebyulpark88@gmail.com>
2023-05-13 13:03:02 +0900

    Feat: Horizontal sliding when prev, next are clicked

--- study-018-transform-2d-motion/js/main.js
+++ study-018-transform-2d-motion/js/main.js
@@ -1,1 +1,25 @@
 'use strict'
+
+import {$} from './Render.js'
+import {eventBind} from "./EventBinding.js";
+
+const frame = $('section')
+const boxes = $('article')
+const prev = $('.prev')
+const next = $('.next')
+
+
+// MARK: Event Binding
+const clickPrev = (target, eventBind) => {
+  eventBind('.prev', 'click', () => {
+    target.prepend(target.lastElementChild)
+  })
+}
+clickPrev(frame, eventBind)
+
+const clickNext = (target, eventBind) => {
+  eventBind('.next', 'click', () => {
+    target.append(target.firstElementChild)
+  })
+}
+clickNext(frame, eventBind)
