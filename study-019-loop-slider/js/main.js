9fe7966492dfe7ff9cc9b97633cbc2b0bac0ac75
sbpark88 <saebyulpark88@gmail.com>
2023-05-13 14:21:18 +0900

    Update: Set style of panel number 3~
    패널 1번: 사라진 패널
    패널 2번: 현재 활성화 패널
    패널 3번 ~: 대기중 패널

--- study-019-loop-slider/js/main.js
+++ study-019-loop-slider/js/main.js
@@ -1,2 +1,21 @@
 'use strict'
 
+import {$} from './Render.js'
+import {eventBind} from "./EventBinding.js";
+
+const frame = $('.list')
+
+// MARK: Event Binding
+const clickPrev = (target, eventBind) => {
+  eventBind('.prev', 'click', () => {
+    target.prepend(target.lastElementChild)
+  })
+}
+clickPrev(frame, eventBind)
+
+const clickNext = (target, eventBind) => {
+  eventBind('.next', 'click', () => {
+    target.append(target.firstElementChild)
+  })
+}
+clickNext(frame, eventBind)
