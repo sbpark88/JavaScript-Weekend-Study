bd42b1f205c883bfee3e49b0136fb7283581996b
sbpark88 <saebyulpark88@gmail.com>
2023-05-14 15:26:16 +0900

    Add: Common Utils (TypeScript ver)

---
+++ study-020-typescript/src/common/EventBinding.ts
@@ -0,0 +1,28 @@
+import {$} from "./Render.ts";
+import {curry} from "./FP.ts";
+
+const _eventBind = (selector: string,
+                    eventType: keyof ElementEventMap,
+                    listener: EventListenerOrEventListenerObject) => {
+  const nodeElement: NodeListOf<Element> | Element = $(selector)
+  switch (true) {
+    case nodeElement instanceof NodeList:
+      return eventBindNodeList(<NodeListOf<Element>>nodeElement, eventType, listener)
+    case nodeElement instanceof Element:
+      return eventBindOneLode(<Element>nodeElement, eventType, listener)
+    default:
+      return
+  }
+}
+
+const eventBindNodeList = (els: NodeListOf<Element>,
+                           eventType: keyof ElementEventMap,
+                           listener: EventListenerOrEventListenerObject) => els.forEach(el => el.addEventListener(eventType, listener))
+const eventBindOneLode = (el: Element,
+                          eventType: string,
+                          listener: EventListenerOrEventListenerObject) => el.addEventListener(eventType, listener)
+const eventBind = curry(_eventBind)
+
+export {
+  eventBind
+}
---
+++ study-020-typescript/src/common/Render.ts
@@ -0,0 +1,52 @@
+import {pipe} from "./FP.ts"
+
+type StringOrArray = string | string[]
+type Position = 'beforebegin' | 'afterbegin' | 'beforeend' | 'afterend'
+
+const insertHTML = (position: Position) => (el: Element) => (html: string) => el.insertAdjacentHTML(position, html)
+
+const insertHtmlBeforeBegin = insertHTML("beforebegin")
+const insertHtmlAfterBegin = insertHTML("afterbegin")
+const insertHtmlBeforeEnd = insertHTML("beforeend")
+const insertHtmlAfterEnd = insertHTML("afterend")
+
+const $ = (selector: string): NodeListOf<Element> | Element => {
+  const nodeList: NodeListOf<Element> = document.querySelectorAll(selector)
+  return nodeList.length === 1 ? nodeList[0] : nodeList
+}
+
+const guardElement = (el: NodeListOf<Element> | Element): boolean => el instanceof Element
+
+const arrayCheck = (something: StringOrArray): boolean => something instanceof Array
+const htmlArrayRemoveComma = (arr: string[]): string => arr.join('')
+const safeHtml = (html: StringOrArray): string => arrayCheck(html) ? htmlArrayRemoveComma(html as string[]) : html as string
+
+const renderBeforeBegin = (el: Element) => (html: StringOrArray) => insertHtmlBeforeBegin(el)(safeHtml(html))
+const renderAfterBegin = (el: Element) => (html: StringOrArray) => insertHtmlAfterBegin(el)(safeHtml(html))
+const renderBeforeEnd = (el: Element) => (html: StringOrArray) => insertHtmlBeforeEnd(el)(safeHtml(html))
+const renderAfterEnd = (el: Element) => (html: StringOrArray) => insertHtmlAfterEnd(el)(safeHtml(html))
+const renderInnerHTML = (el: Element) => (html: StringOrArray) => el.innerHTML = safeHtml(html)
+
+const renderWithTemplate = (selector: string) => (template: (item: {}) => string) => (jsonData: string[]) => {
+  const nodeElement: NodeListOf<Element> | Element = $(selector)
+  if (!guardElement(nodeElement)) return
+  const attachmentTarget: Element = nodeElement as Element
+  const renderWithTemplate = renderInnerHTML(attachmentTarget)
+  const createJsonToHtml = (jsonData: string[]): StringOrArray => jsonData.map(item => template(item))
+
+  return pipe(
+      createJsonToHtml,
+      renderWithTemplate
+  )(jsonData)
+}
+
+export {
+  $,
+  guardElement,
+  renderBeforeBegin,
+  renderAfterBegin,
+  renderBeforeEnd,
+  renderAfterEnd,
+  renderInnerHTML,
+  renderWithTemplate
+}
