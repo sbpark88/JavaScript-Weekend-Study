c49fe13525ea506f30b83aaff998e38372f68794
sbpark88 <saebyulpark88@gmail.com>
2023-05-29 23:01:36 +0900

    Update: Infinite scroll(call data by intersection observer)

--- study-009-infinite-scroll/js/main.js
+++ study-009-infinite-scroll/js/main.js
@@ -1,11 +1,39 @@
+const cardWrap = document.querySelector('section')
 const cards = document.querySelectorAll('article')
+
+// MARK: Observer
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     entry.target.classList.toggle('on', entry.isIntersecting)
   })
 }, {
-  // threshold: .5
-  rootMargin: '-140px'
+  threshold: .5
+  // rootMargin: '-140px'
 })
 
 cards.forEach(card => observer.observe(card))
+
+function loadCards() {
+  let i = 1
+  do {
+    const card = document.createElement('article')
+    card.classList.add(`card${i}`)
+    card.textContent = 'Box Created'
+    observer.observe(card)
+    cardWrap.append(card)
+    i++
+  } while (i < 9)
+  attachLoadCards()
+}
+
+// MARK: Call more card observer (observing last card)
+const lastCardObserver = new IntersectionObserver((entries) => {
+  const lastCard = entries[0]
+  if (lastCard.isIntersecting) {
+    loadCards()
+    lastCardObserver.unobserve(lastCard.target)
+  }
+})
+
+const attachLoadCards = () => lastCardObserver.observe(document.querySelector('article:last-child'))
+attachLoadCards()
