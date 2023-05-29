9e6f747784a08a01d820f807f383ececc27ba78f
sbpark88 <saebyulpark88@gmail.com>
2023-05-29 21:02:41 +0900

    Update: Change 'threshold' to 'rootMargin'

--- study-009-infinite-scroll/js/main.js
+++ study-009-infinite-scroll/js/main.js
@@ -1,12 +1,11 @@
 const cards = document.querySelectorAll('article')
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      observer.unobserve(entry.target)
-      entry.target.classList.toggle('on', entry.isIntersecting)
-    }
+    entry.target.classList.toggle('on', entry.isIntersecting)
   })
-}, {})
+}, {
+  // threshold: .5
+  rootMargin: '-140px'
+})
 
 cards.forEach(card => observer.observe(card))
-
